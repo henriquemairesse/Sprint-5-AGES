7863b61cbb436ca7f9d4589d3f4a66a71c201f65
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-05-07 16:47:03 -0300

    1v

--- Portifolio/src/components/ProjectCard.tsx
+++ Portifolio/src/components/ProjectCard.tsx
@@ -36,42 +36,48 @@ const ProjectCard: React.FC<ProjetoProps> = ({ nomeProjeto }) => {
 
   return (
     <div className='projectCard'>
-        <div className='projectCardHeader'>
-            <img src={projeto.imagemSrc} alt={projeto.alt} className="projectCardImage" ></img>
-            <p className='projectCardTitle'>{projeto.titulo}</p>
+      <div className='projectCardHeader'>
+        <img src={projeto.imagemSrc} alt={projeto.alt} className="projectCardImage" ></img>
+        <p className='projectCardTitle'>{projeto.titulo}</p>
+      </div>
+      <div className={`projectCardExpanded ${expanded ? 'expanded' : ''}`}>
+        <p className="projectCardDescription">{projeto.descricao}</p>
+        <div className="projectCardLinks">
+          {projeto.linkGithub && (
+            <a
+              href={projeto.linkGithub}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkGithub"
+            >
+              Repositório
+            </a>
+          )}
+          {projeto.linkProjeto && (
+            <a
+              href={projeto.linkProjeto}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkProjeto"
+            >
+              Ver Projeto
+            </a>
+          )}
         </div>
-        {expanded && (
-            <div className="projectCardExpanded">
-                <p className="projectCardDescription">{projeto.descricao}</p>
-                <div className="projectCardLinks">
-                    {projeto.linkGithub && (
-                    <a
-                        href={projeto.linkGithub}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="linkGithub"
-                    >
-                        Repositório
-                    </a>
-                    )}
-                    {projeto.linkProjeto && (
-                    <a
-                        href={projeto.linkProjeto}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="linkProjeto"
-                    >
-                        Ver Projeto
-                    </a>
-                    )}
-                </div>
-            </div>
-        )}
-        <img className="projectCardButton" src="./images/arrowButtonImage.png" style={{ 
-          transform: `rotate(${expanded ? 180 : 0}deg) scale(${isHovering ? 1.15 : 1}) `, transition: 'transform 0.3s ease-in-out' }}
-          onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} onClick={toggleExpand}/>
+      </div>
+      <img
+        className="projectCardButton"
+        src="./images/arrowButtonImage.png"
+        style={{
+          transform: `rotate(${expanded ? 180 : 0}deg) scale(${isHovering ? 1.2 : 1})`,
+          transition: 'transform 0.3s ease-in-out',
+        }}
+        onClick={toggleExpand}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
