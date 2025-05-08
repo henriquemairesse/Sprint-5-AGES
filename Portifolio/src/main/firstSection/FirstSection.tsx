87f1611f57602f07142f58c249689bdac943603e
MImortal <hvmkiki@gmail.com>
2025-05-08 00:45:50 -0300

    08.05-10

--- Portifolio/src/main/firstSection/FirstSection.tsx
+++ Portifolio/src/main/firstSection/FirstSection.tsx
@@ -6,7 +6,7 @@ function FirstSection() {
     const agora = new Date();
     const hora = agora.getHours();
   
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 1 && hora < 12) {
       return "Bom dia";
     } else if (hora >= 12 && hora < 18) {
       return "Boa tarde";
