07ca96416d87560b442c767ac76c6d67ee882a32
Henrique Mairesse <82916381+henriquemairesse@users.noreply.github.com>
2025-05-08 16:34:36 -0300

    Update Section.tsx

--- Portifolio/src/components/Section.tsx
+++ Portifolio/src/components/Section.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-scroll';
 
 interface Itens {
-  sectionId: string; 
+  sectionId: string;
   text: string;
 }
 
 const Section: React.FC<Itens> = ({ sectionId, text }) => {
+  const isInitialMobile = window.innerWidth < 700;
+
   return (
     <li>
-      <Link activeClass="active" to={sectionId} smooth={true} duration={500} offset={-5 * (window.innerWidth / 100)} className="Section">{text}</Link>
+      <Link activeClass="active" to={sectionId} smooth={true} duration={500} offset={(isInitialMobile ? -10 : -5) * (window.innerWidth / 100)} className="Section">{text}</Link>
     </li>
   );
 };
